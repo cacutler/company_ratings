5dc448a4cdbdab9589b1be122a4399931c6fe416
cacutler <116305038+cacutler@users.noreply.github.com>
2024-06-20 12:29:54 -0600

    Initialized the five http methods

--- server.js
+++ server.js
@@ -4,6 +4,21 @@ const model = require("./model")
 const cors = require("cors")
 app.use(cors())
 app.use(express.urlencoded({extended: true}))
+app.get("/company-ratings", async (request, response) => {
+
+})
+app.get("/company-ratings/:id", async (request, response) => {
+
+})
+app.post("/company-ratings", async (request, response) => {
+
+})
+app.delete("/company-ratings/:id", async (request, response) => {
+
+})
+app.put("/company-ratings/:id", async (request, response) => {
+
+})
 app.listen(8080, () => {
     console.log("Server listening on http://localhost:8080.")
 })
