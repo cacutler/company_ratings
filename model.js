314390884bb9ba891072960e46b515ceb96971c8
cacutler <116305038+cacutler@users.noreply.github.com>
2024-06-20 12:24:02 -0600

    Created a data model

--- model.js
+++ model.js
@@ -1,2 +1,5 @@
 const mongoose = require("mongoose")
 mongoose.connect(process.env.DBPASSWORD)
+const CompanyRatingSchema = new mongoose.Schema({name: {type: String, required: [true, "Company must have a name."]}, rating: {type: Number, required: [true, "Company must have a rating."]}, review: {type: String, required: [true, "Company must have a review."]}})
+const CompanyRating = mongoose.model("CompanyRating", CompanyRatingSchema)
+module.exports = {CompanyRating: CompanyRating}
